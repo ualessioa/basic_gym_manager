12c7d612c2f5243557667ad11ed300f5e5b816b7
Alessio Ardito <ualessioa@gmail.com>
2022-07-08 11:14:12 +0200

    First experimental commit

--- well.py
+++ well.py
@@ -1,3 +1,6 @@
+class Palestra:
+    def __init__(self, nome, nr_iscritti = 0, abbonamenti = {}, corsi = [])
+
 print(f"Benvenuto in Wellness Manager, cosa vorresti fare?")
 master_input = input("> ")
 while not master_input == "esci":
